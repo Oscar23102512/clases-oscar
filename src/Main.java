a08ef63e591cf317351ca545b86f0f62b1f58ce5
pablez <pablogr2928@gmail.com>
2025-07-14 21:53:07 -0400

    clase 1

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        // Tema #1 varibles, tipos de datos,
+        // Tema #2 datos de entrada y salida
+        // Tema #3 artimetica,
+        // Tema #4 simbolos condicionales,
+        // Tema #5 condicionles(IF),
+        // Tema #6 bucles (While, Do while, For, For Each),
+        // Tema #7 vectores y matrices
+        // Tema #8 menus (swhich)
+        // Tema #9 funciones
+        // Tema #10 clases
+
+        //--------------Variables--------------------
+        // ----------Tipos de Datos------------------
+        int numeros = 15;
+        int edad = 25;
+        int peso = 85;
+
+
+    }
+}
